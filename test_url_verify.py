7ebc0e253045750d2c6b41c0ad3bb88812611015
APalhade4 <apalhade4@gmail.com>
2023-01-25 08:43:22 +0530

    ACIDT-13 Constraints Pavan_Commit-2

--- test_url_verify.py
+++ test_url_verify.py
@@ -3,3 +3,12 @@ list = [10, 20, 30, 40, 50, 60]
 for vedang in list:
     print(vedang)
 
+list = [10, 20, 30, 40, 50, 60]
+
+for vedang in list:
+    print(vedang)
+
+    list = [10, 20, 30, 40, 50, 60]
+
+    for vedang in list:
+        print(vedang)
